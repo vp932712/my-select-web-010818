4795ec832587fa7fb58b50f2a2dced6bbb6ab8c8
Vaibhav Patel <vp932712@gmail.com>
2017-12-13 22:15:15 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+empty = []
+if block_given?
+  i = 0 
+  while i < collection.length
+    yield (collection[i])
+    empty.push(collection.even?)
+  end
+  empty
+
+ 
 end
