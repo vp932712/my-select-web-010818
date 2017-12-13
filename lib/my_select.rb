73569b8b7ebe58732955b16793a98ddd20fea4e3
Vaibhav Patel <vp932712@gmail.com>
2017-12-13 22:15:51 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,12 +2,12 @@ def my_select(collection)
  # your code here!
 empty = []
 if block_given?
-  i = 0 
+  i = 0
   while i < collection.length
-    yield (collection[i])
-    empty.push(collection.even?)
+    yield (num[i])
+    empty.push(num.even?)
   end
   empty
 
- 
+
 end
