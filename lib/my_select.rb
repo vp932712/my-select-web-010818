03293cdcdbae1ef3105ee16d44580acfb8864be3
Vaibhav Patel <vp932712@gmail.com>
2017-12-13 22:22:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
  # your code here!
+empty = []
+if block_given?
+  i = 0
+  while i < collection.length
+    yield (nums[i])
+    if (nums[i].even?)
+      empty.push(nums[i])
+    end
+    i +=1
+  end
+empty
+else
+  "no block given"
+end
+
 end
